10c7c2e4c340906bdf8dac8a65e12513d955ce4e
QuantumNovice <portabl3lapy@gmail.com>
2019-11-27 23:11:08 +0500

    Skeltal UI working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from "jquery"
+
+// Wouldn't it be better to use react-bootstrap-sidebar
+import cat from './imgs/cat.png';
+import grumpy_cat from './imgs/grumpy-cat.jpg';
+import doge from './imgs/doge.jpg';
+
+const imgs = [cat, grumpy_cat, doge, cat, grumpy_cat, doge,cat, grumpy_cat, doge,cat, grumpy_cat, doge];
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="Meme Maker">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {window.pageXOffset}
+        {window.pageYOffset}
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="progress">
+                <div className="progress-bar w-75">
+                </div>
+              </div>
+              <ul className="nav">
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Popular</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">Contact</a>
+                </li>
+
+              </ul>
+              <div className="page-header">
+                <h1>
+                  Meme Maker
+                </h1>
+              </div>
+              <div className="row">
+
+                {imgs.map((value, index) => {
+                  return <div className="col-md-4">
+                    <div className="card">
+                    <img className="card-img-top" alt="{index}" src={value} />
+                    <div className="card-block">
+                      <h5 className="text">
+                        {index}
+                      </h5>
+
+                      <p>
+                        <a className="btn btn-primary" href="#">Make Meme</a>
+                        <a className="btn" href="#">Generate</a>
+                      </p>
+                    </div>
+                  </div>
+                  </div>
+                })}
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </header>
     </div>
   );
