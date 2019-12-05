d2934b0bde607cb43fa475c5402b29eff034c78d
MianJawadAhmad <a.jawad321@gmail.com>
2019-12-05 13:32:44 +0500

    envirment setting

--- src/App.js
+++ src/App.js
@@ -2,28 +2,21 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-import Home from './component/Home'
-// Wouldn't it be better to use react-bootstrap-sidebar
-// As of now the navbar ain't working. FIX IT
-
-// import meme from './memes.json'
-// import cat from './imgs/cat.png';
-// import grumpy_cat from './imgs/grumpy-cat.jpg';
-// import doge from './imgs/doge.jpg';
-
-//  const memes = axios.get('./memes.json')
-//  const memes = JSON.parse(meme);
-//  alert(memes.Cat);
-
-// const names =["Cat", "Grumpy Cat", "Doge"];
-// const imgs = [cat, grumpy_cat, doge];
+import Home from './component/Home';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Textedit from './component/Textedit';
 
 
-function App() {
+const App = ()=> {
 
   return (
-    <Home />
+      //Route of homejs and textedit js
+    <div>
+    <BrowserRouter>
+<Route path="/" exact component = {Home}></Route>
+<Route path="/textedit" component = {Textedit}></Route>
+    </BrowserRouter>
+</div>
   );
 }
 
--- src/component/Home.js
+++ src/component/Home.js
@@ -3,12 +3,12 @@ import './css/home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DISHES } from '../memes.js'
 import Header from './header'
-import CardMedia from '@material-ui/core/CardMedia';
-import { withStyles } from '@material-ui/core/styles';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import { Card, CardContent } from "@material-ui/core";
-import Textedit from './Textedit'
-
+// import CardMedia from '@material-ui/core/CardMedia';
+// import { withStyles } from '@material-ui/core/styles';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import { Card, CardContent } from "@material-ui/core";
+// import Textedit from './Textedit';
+import {Link} from 'react-router-dom';
 
 
 class Home extends Component {
@@ -26,66 +26,71 @@ class Home extends Component {
 
   render() {
 
-    let rows = DISHES.map((value, index) => {
-      return <Card className={styles.card}>
-        <div className={styles.con}>
-          <CardContent className={styles.content}>
-            <div style={{ position: 'relative' }} >
-              <CardMedia
-                component="img"
-                className={styles.media}
-                image={value.img}
-              />
-              <div style={{ position: 'absolute', color: 'white', top: 8, left: '50%', transform: 'translateX(-50%)' }} >
-                <form onSubmit={this.handleSubmit}>
-                  <label>
-                    <input type="text" placeholder="Top Text" value={this.state.value} onChange={this.handleChange} />
-                  </label>
-                </form>
-              </div>
-              <div style={{ position: 'absolute', color: 'white', top: '90%', left: '50%', transform: 'translateX(-50%)' }} >
-                <form onSubmit={this.handleSubmit}>
-                  <label>
-                    <input type="text" placeholder="Bottom Text" value={this.state.value} onChange={this.handleChange} />
-                  </label>
-                </form>
-              </div>
-            </div>
-            <div className="card-block">
-              <h5 className="text">
-                {value.title}
-              </h5>
-              <p>
-                <a className="btn btn-primary" href="#">Make Meme</a>
-                <a className="btn" href="#">Download</a>
-              </p>
-            </div>
-          </CardContent>
-        </div>
-      </Card>
 
+   const rows = DISHES.map((value, index) => {
+       return       <div className="col-md-4">
+       <div className="card">
+         <img className="card-img-top" alt={"{names[index]}"} src={value.img} />
+         {/* <Textedit/> */}
+         <div className="card-block">
+           <h5 className="text">
+             {value.title}
+           </h5>
+        < div>
+             <Link className="btn btn-primary" to={`./textedit/${value.id}`}>Make Meme</Link>
+             <Link className="btn" to="#">Download</Link>
+             </div>
+         </div>
+       </div>
+     </div>
 
-
-
-      // <div className="col-md-4">
-      //   <div className="card">
-      //     <img className="card-img-top" alt="{names[index]}" src={value.img} />
-      //     <Textedit/>
-      //     <div className="card-block">
-      //       <h5 className="text">
-      //         {value.title}
-      //       </h5>
-      //       <p>
-      //         <a className="btn btn-primary" href="#">Make Meme</a>
-      //         <a className="btn" href="#">Download</a>
-      //       </p>
-      //     </div>
+      
+       // <Card className={styles.card}>
+      //   <div className={styles.con}>
+      //     <CardContent className={styles.content}>
+      //       <div style={{ position: 'relative' }} >
+      //         <CardMedia
+      //           component="img"
+      //           className={styles.media}
+      //           image={value.img}
+      //         />
+      //         <div style={{ position: 'absolute', color: 'white', top: 8, left: '50%', transform: 'translateX(-50%)' }} >
+      //           <form onSubmit={this.handleSubmit}>
+      //             <label>
+      //               <input type="text" placeholder="Top Text" value={this.state.value} onChange={this.handleChange} />
+      //             </label>
+      //           </form>
+      //         </div>
+      //         <div style={{ position: 'absolute', color: 'white', top: '90%', left: '50%', transform: 'translateX(-50%)' }} >
+      //           <form onSubmit={this.handleSubmit}>
+      //             <label>
+      //               <input type="text" placeholder="Bottom Text" value={this.state.value} onChange={this.handleChange} />
+      //             </label>
+      //           </form>
+      //         </div>
+      //       </div>
+      //       <div className="card-block">
+      //         <h5 className="text">
+      //           {value.title}
+      //         </h5>
+      //         <p>
+      //           <a className="btn btn-primary" href="#">Make Meme</a>
+      //           <a className="btn" href="#">Download</a>
+      //         </p>
+      //       </div>
+      //     </CardContent>
       //   </div>
-      // </div>
+      // </Card>
+
+
+
+
+
     })
 
 
     return (
+ 
       <div className="Meme Maker">
         <Header />
         <div className="row">
